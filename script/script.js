e6388f4d83831c8c111394f051076a786bab720c
hakyeongjun <has7856@naver.com>
2022-05-19 15:00:56 +0900

    update

--- script/script.js
+++ script/script.js
@@ -15,7 +15,24 @@ $(function(){
         },20);
     });
 
-    $(".card-img1").hover(function(){
-        $(".cover",this).fadeToggle();
+    $(document).scroll(function(){
+        var winH = $(window).height();
+        if($(this).scrollTop() >= winH){
+            $("#menu-tab").addClass("fixed");
+        }else{
+            $("#menu-tab").removeClass("fixed");
+        }
     });
+
+    var dday = new Date("May 23, 2022, 0:00:00").getTime();
+    setInterval(function() {
+        var today = new Date().getTime();
+        var gap = dday - today;
+        var day = Math.ceil(gap / (1000 * 60 * 60 * 24));
+        var hour = Math.ceil((gap % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var min = Math.ceil((gap % (1000 * 60 * 60)) / (1000 * 60));
+        var sec = Math.ceil((gap % (1000 * 60)) / 1000);
+      
+        $(".count").html("D-" + day);
+      }, 1000);
 });
